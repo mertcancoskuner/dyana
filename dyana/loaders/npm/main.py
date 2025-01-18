eed3260c2f0fa3f9a8c504ec6316a1bade8e4177
evilsocket <evilsocket@gmail.com>
2025-01-18 15:47:58 +0100

    new: npm loader explicitly loads the package after installation

--- dyana/loaders/npm/main.py
+++ dyana/loaders/npm/main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import re
 import subprocess
 
 from dyana import Profiler  # type: ignore[attr-defined]
@@ -14,6 +15,15 @@ if __name__ == "__main__":
         subprocess.check_call(["npm", "install", args.package])
         profiler.track_memory("after_installation")
         profiler.track_disk("after_installation")
+
+        # explicitly require the package to make sure it's loaded
+        package_name = re.split("[^a-zA-Z0-9_-]", args.package)[0]
+        result = subprocess.run(["node", "-e", f"require('{package_name}')"], capture_output=True, text=True)
+
+        profiler.track("require_exit_code", result.returncode)
+        profiler.track("require_stdout", result.stdout)
+        profiler.track("require_stderr", result.stderr)
+
     except Exception as e:
         profiler.track_error("npm", str(e))
 
